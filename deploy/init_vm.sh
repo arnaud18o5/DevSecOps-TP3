8cf322ff658743fb5dd240853509a90bdad8ae73
Alexis Malosse <alexis.malosse@etu.unice.fr>
2024-11-18 21:29:11 +0100

    init vm script

---
+++ deploy/init_vm.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Mettre à jour les paquets
+sudo apt update
+
+# Installer PHP et les extensions nécessaires
+sudo apt install -y php
+sudo apt install -y php-{cli,curl,mbstring,xml,zip,gd}
+sudo systemctl restart apache2
+
+# Installer Composer si ce n'est pas déjà fait
+if [ ! -f /usr/local/bin/composer ]; then
+    # Télécharger le script d'installation de Composer
+    curl -sS https://getcomposer.org/installer -o composer-setup.php
+
+    # Vérifier l'intégrité du script d'installation
+    HASH=$(curl -sS https://composer.github.io/installer.sig)
+    if php -r "exit(hash_file('SHA384', 'composer-setup.php') === '$HASH' ? 0 : 1;"; then
+    echo "Installer verified"
+    else
+    echo "Installer corrupt"
+    rm composer-setup.php
+    exit 1
+    fi
+
+    # Installer Composer
+    sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+    rm composer-setup.php
+fi
+
+# Cloner le dépôt Git et configurer le projet
+cd ~
+if ! [ -d prod ]; then
+    git clone https://github.com/arnaud18o5/DevSecOps-TP3.git
+    mv DevSecOps-TP3/ prod
+    cd prod/
+    git config pull.rebase false  # merge
+fi
+
+echo "Installation terminée avec succès !"
